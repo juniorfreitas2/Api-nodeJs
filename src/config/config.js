c54b650d2ce6e0f8c9f0f5f9a5ef55fcced83a75
Junior <web.juniorfreitas@gmail.com>
2019-05-06 20:46:12 -0500

    first commit

---
+++ src/config/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    development: {
+      database: {
+        host: "localhost",
+        port: 3306,
+        name: "spoiler",
+        dialect: "mysql",
+        user: "root",
+        password: "123"
+      }
+    },
+    production: {
+      database: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        name: "",
+        user: "",
+        password: ""
+      }
+    }
+  };
